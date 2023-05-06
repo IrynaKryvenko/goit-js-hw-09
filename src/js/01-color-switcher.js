9cf36cba90c87d5c94821fec9cf704a8240fca8a
IrynaKryvenko <kryvenkoirina93@gmail.com>
2023-05-06 23:31:07 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),    
+}
+ 
+let timerId = null;
+refs.stopBtn.disabled = true;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function changeColor() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+function onStartBtnClick() {
+    timerId = setInterval(() => {
+    changeColor();
+    }, 1000);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+};
+
+function onStopBtnClick() {
+    clearInterval(timerId);
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+}
