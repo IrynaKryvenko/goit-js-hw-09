e6ee932d54cc1f53ad38b4c9a9a5d0099f3b61eb
IrynaKryvenko <kryvenkoirina93@gmail.com>
2023-05-11 16:52:08 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,6 +9,45 @@ const minutesTimer = document.querySelector('span[data-minutes]');
 const secondsTimer = document.querySelector('span[data-seconds]');
 let timerId = null;
 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function startTimer(selectedDate) {
+  timerId = setInterval(() => {
+    buttonStart.disabled = true;
+    const difference = selectedDate.getTime() - new Date().getTime();
+    if (difference < 1000) {
+      clearInterval(timerId);
+      secondsTimer.textContent = '00';
+    } else {
+      daysTimer.textContent = addLeadingZero(convertMs(difference).days);
+      hoursTimer.textContent = addLeadingZero(convertMs(difference).hours);
+      minutesTimer.textContent = addLeadingZero(convertMs(difference).minutes);
+      secondsTimer.textContent = addLeadingZero(convertMs(difference).seconds);
+    }
+  }, 1000);
+}
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -18,52 +57,10 @@ const options = {
     console.log(selectedDates[0]);
     if (new Date().getTime() > selectedDates[0].getTime()) {
       window.alert('Please choose a date in the future');
-      
     } else {
       buttonStart.disabled = false;
       buttonStart.addEventListener('click', () => {
-        timerId = setInterval(() => {
-          buttonStart.disabled = true;
-          const difference = selectedDates[0].getTime() - new Date().getTime();
-          if (difference < 1000) {
-            clearInterval(timerId);
-            secondsTimer.textContent = '00';
-          } else {
-            function convertMs(ms) {
-              // Number of milliseconds per unit of time
-              const second = 1000;
-              const minute = second * 60;
-              const hour = minute * 60;
-              const day = hour * 24;
-
-              // Remaining days
-              const days = Math.floor(ms / day);
-              // Remaining hours
-              const hours = Math.floor((ms % day) / hour);
-              // Remaining minutes
-              const minutes = Math.floor(((ms % day) % hour) / minute);
-              // Remaining seconds
-              const seconds = Math.floor(
-                (((ms % day) % hour) % minute) / second
-              );
-
-              return { days, hours, minutes, seconds };
-            }
-            daysTimer.textContent = addLeadingZero(convertMs(difference).days);
-            hoursTimer.textContent = addLeadingZero(
-              convertMs(difference).hours
-            );
-            minutesTimer.textContent = addLeadingZero(
-              convertMs(difference).minutes
-            );
-            secondsTimer.textContent = addLeadingZero(
-              convertMs(difference).seconds
-            );
-            function addLeadingZero(value) {
-              return String(value).padStart(2, '0');
-            }
-          }
-        }, 1000);
+        startTimer(selectedDates[0]);
       });
     }
   },
